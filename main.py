ad8989cef64c6fc4405b9c06e40ba49a18574b8f
jackschu <js9pa@virginia.edu>
2020-05-25 22:38:20 -0400

    blackened

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from board import Board
 
+
 class App:
     def __init__(self):
         self._board = Board()
@@ -11,16 +12,22 @@ class App:
 
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._display_surf = pygame.display.set_mode(
+            self.size, pygame.HWSURFACE | pygame.DOUBLEBUF
+        )
         self._running = True
+
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
+
     def on_loop(self):
         pass
+
     def on_render(self):
         self._board.draw(self._display_surf)
         pygame.display.update()
+
     def on_cleanup(self):
         pygame.quit()
 
@@ -28,13 +35,14 @@ class App:
         if self.on_init() == False:
             self._running = False
 
-        while( self._running ):
+        while self._running:
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
             self.on_render()
         self.on_cleanup()
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     theApp = App()
     theApp.on_execute()
