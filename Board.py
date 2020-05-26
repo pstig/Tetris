bb89344e6f7d95f3836fa75d2d7bc230ef184374
jackschu <js9pa@virginia.edu>
2020-05-25 23:56:30 -0400

    drawing piece

--- Board.py
+++ Board.py
@@ -4,6 +4,14 @@ import random
 
 
 class Board:
+    piece_to_color_idx = {"I": 1,
+                          "J": 2,
+                          "L": 3,
+                          "O": 4,
+                          "S": 5,
+                          "T": 6,
+                          "Z": 7,
+    }
     colors = [
         pygame.Color("white"),
         pygame.Color("cyan"),
@@ -39,6 +47,15 @@ class Board:
             for i in range(self.row_count):
                 if self._tiles[i][j] != 0:
                     self.fill_tile(surface, j, i, Board.colors[self._tiles[i][j]])
+        piece_offsets = self.active_piece.get_locations()
+        piece_color_idx = Board.piece_to_color_idx[self.active_piece.get_character_name()]
+        for offset in piece_offsets:
+            self.fill_tile(
+                surface,
+                self.active_piece_location[0] + offset[0],
+                self.active_piece_location[1] + offset[1],
+                Board.colors[piece_color_idx],
+            )
 
     def clear_rows(self):
         remove_count = 0
