a2357a7341f253507f796192423af191758e8ef5
jackschu <js9pa@virginia.edu>
2020-05-26 00:40:00 -0400

    J piece

---
+++ JPiece.py
@@ -0,0 +1,23 @@
+from Piece import Piece
+
+class JPiece(Piece):
+    def __init__(self):
+        self._positions = [[(0,0),(0,1),(0,-1),(-1,-1)],
+                          [(0,0),(1,0),(-1,0),(-1,1)],
+                          [(0,0),(0,1),(0,-1),(1,1)],
+                          [(0,0),(1,0),(-1,0),(1,-1)],
+        ]
+        self._position_id = 0
+        super().__init__()
+
+    def rotate_left(self):
+        self._position_id = (self._position_id - 1) % 4
+
+    def rotate_right(self):
+        self._position_id = (self._position_id + 1) % 4
+
+    def get_locations(self):
+        return self._positions[self._position_id]
+
+    def get_character_name(self):
+        return 'J'
